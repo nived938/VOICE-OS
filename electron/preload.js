f230ff0a3b6bef04ae83dab57219bfb4ab1f7842
nived938 <nivedsreejaharidasan@gmail.com>
2026-08-27 15:16:23 +0530

    Add secure renderer bridge for computer controls

---
+++ electron/preload.js
@@ -0,0 +1,14 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('voiceOS', {
+  open: (target) => ipcRenderer.invoke('system:open', target),
+  openPath: (target) => ipcRenderer.invoke('system:open-path', target),
+  reveal: (target) => ipcRenderer.invoke('system:reveal', target),
+  searchFiles: (query) => ipcRenderer.invoke('system:search-files', query),
+  readFile: (target) => ipcRenderer.invoke('system:read-file', target),
+  writeTextFile: (target, content) => ipcRenderer.invoke('system:write-text-file', target, content),
+  clipboard: (text) => ipcRenderer.invoke('system:clipboard', text),
+  power: (action) => ipcRenderer.invoke('system:power', action),
+  devtools: () => ipcRenderer.invoke('app:toggle-devtools'),
+  onHotkey: (callback) => ipcRenderer.on('voice:hotkey', callback)
+});
